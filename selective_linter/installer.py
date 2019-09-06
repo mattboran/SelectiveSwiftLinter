7bfb43cd27392262bb9c31588288e1013cb8cc19
Matt Boran <mattbo@zillowgroup.com>
2019-09-06 16:34:37 -0400

    Added path sourcing to precommit hook so that it works in SourceTree, and also refactored installer to be more 'clean code'-ish

--- selective_linter/installer.py
+++ selective_linter/installer.py
@@ -4,6 +4,7 @@ import sh
 
 HOOK = """
 #!/usr/bin/bash
+PATH=$PATH:/usr/local/bin:/usr/local/sbin
 skipswiftlint=$(git config --bool hooks.skipswiftlint)
 if [ "$skipswiftlint" != "true" ]
 then
@@ -23,7 +24,10 @@ else
     exit 0
 fi
 """
-        
+PRE_COMMIT = '/.git/hooks/pre-commit'
+GITIGNORE = '.gitignore'
+LINT_CACHE = '.lint_cache'
+
 class Installer:
 
     def __init__(self, dir):
@@ -37,14 +41,38 @@ class Installer:
         return True
 
     def install(self): 
-        pre_hook_dir = self.dir + '/.git/hooks/pre-commit'
+        pre_hook_dir = self.dir + PRE_COMMIT
         should_confirm = os.path.exists(pre_hook_dir)
         if should_confirm:
             answer = input("Over-write existing pre-commit hook in this repository? (Y/n) ") or "n"
             if answer[0].lower() == "n":
                 return
-        with open(pre_hook_dir, 'w+') as pre_commit_hook:
+        self._write_pre_commit_hook()
+        self._make_pre_commit_hook_executable()
+        self._add_lint_cache_to_gitignore()
+
+    @property
+    def pre_hook_dir(self):
+        return self.dir + PRE_COMMIT
+
+    @property
+    def gitignore_dir(self):
+        return self.dir + GITIGNORE
+
+    def _write_pre_commit_hook(self): 
+        with open(self.pre_hook_dir, 'w+') as pre_commit_hook:
             pre_commit_hook.write(HOOK)
         print("Wrote hook to {}".format(pre_hook_dir))
-        sh.chmod('744', pre_hook_dir)
+
+    def _make_pre_commit_hook_executable(self)
+        sh.chmod('744', self.pre_hook_dir)
         print("Made hook executable")
+
+    def _add_lint_cache_to_gitignore(self):
+        should_add_to_gitignore = False
+        with open(self.gitignore_dir, 'r') as gitignore:
+            if LINT_CACHE not in gitignore.read()
+                should_add_to_gitignore = True
+        if should_add_to_gitignore:
+            with open(self.gitignore_dir, 'a') as gitignore:
+                gitignore.write(LINT_CACHE)
--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="selective_linter",
-    version="0.0.2",
+    version="0.0.3",
     author="Matt Boran",
     author_email="mattboran@gmail.com",
     description="A script to be used as a pre-commit git hook for projects written in Swift",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mattboran/SelectiveSwiftLinter",
-    download_url="https://github.com/mattboran/SelectiveSwiftLinter/releases/download/0.0.2/selective_linter-0.0.2-py3-none-any.whl",
+    download_url="https://github.com/mattboran/SelectiveSwiftLinter/releases/download/0.0.3/selective_linter-0.0.3-py3-none-any.whl",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -23,5 +23,5 @@ setuptools.setup(
         'colorama',
         'sh'
     ],
-    python_requires='>3.0.0'
+    python_requires='>3.0.0',
 )
